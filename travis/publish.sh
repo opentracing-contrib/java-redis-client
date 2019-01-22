0b1bc5d9859f040592cdf692c3160316fdbebdc4
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-01-23 00:32:27 +0800

    less logs during the build (#30)

--- travis/publish.sh
+++ travis/publish.sh
@@ -108,7 +108,7 @@ if ! is_pull_request && build_started_by_tag; then
   check_release_tag
 fi
 
-./mvnw install -nsu
+./mvnw install -nsu -q
 
 # If we are on a pull request, our only job is to run tests, which happened above via ./mvnw install
 if is_pull_request; then
@@ -116,15 +116,15 @@ if is_pull_request; then
 # If we are on master, we will deploy the latest snapshot or release version
 #   - If a release commit fails to deploy for a transient reason, delete the broken version from bintray and click rebuild
 elif is_travis_branch_master; then
-  ./mvnw --batch-mode -s ./.settings.xml -Prelease -nsu -DskipTests deploy
+  ./mvnw --batch-mode -s ./.settings.xml -Prelease -nsu -q -DskipTests deploy
 
   # If the deployment succeeded, sync it to Maven Central. Note: this needs to be done once per project, not module, hence -N
   if is_release_commit; then
-    ./mvnw --batch-mode -s ./.settings.xml -nsu -N io.zipkin.centralsync-maven-plugin:centralsync-maven-plugin:sync
+    ./mvnw --batch-mode -s ./.settings.xml -nsu -q -N io.zipkin.centralsync-maven-plugin:centralsync-maven-plugin:sync
   fi
 
 # If we are on a release tag, the following will update any version references and push a version tag for deployment.
 elif build_started_by_tag; then
   safe_checkout_master
-  ./mvnw --batch-mode -s ./.settings.xml -Prelease -nsu -DreleaseVersion="$(release_version)" -Darguments="-DskipTests" release:prepare
+  ./mvnw --batch-mode -s ./.settings.xml -Prelease -nsu -q -DreleaseVersion="$(release_version)" -Darguments="-DskipTests" release:prepare
 fi
