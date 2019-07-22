66a2c8e94b24885e1d0dc7560d1bc4de7b432865
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-07-22 20:34:03 +0800

    add lettuce 5.0
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ opentracing-redis-lettuce5.0/src/main/java/io/opentracing/contrib/redis/lettuce/CompletableRedisFuture.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017-2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentracing.contrib.redis.lettuce;
+
+import io.lettuce.core.RedisFuture;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableRedisFuture<T> extends CompletableFuture<T> implements RedisFuture<T> {
+
+  private RedisFuture<T> wrappedFuture;
+
+  public CompletableRedisFuture(RedisFuture<T> wrappedFuture) {
+    this.wrappedFuture = wrappedFuture;
+  }
+
+  @Override
+  public String getError() {
+    return wrappedFuture.getError();
+  }
+
+  @Override
+  public boolean await(long timeout, TimeUnit unit) throws InterruptedException {
+    return wrappedFuture.await(timeout, unit);
+  }
+}
